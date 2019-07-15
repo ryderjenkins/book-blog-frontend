eafe170803935e3aef94c0a9786767a015dabcc2
ryderjenkins <ryderjenkins11@gmail.com>
2019-07-15 16:33:28 +1000

    delete json.lock and fix routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Homepage from './container/HomePage';
-import Showpage from './container/ShowPage';
+import HomePage from './container/HomePage';
+import ShowPage from './container/ShowPage';
 import TopPicks from './container/TopPicks';
 import SortByYear from './container/SortByYear';
 import SortByGenre from './container/SortByGenre';
@@ -18,8 +18,8 @@ render() {
 
   return (
     <Switch>
-      <Route path="/home" exact component={Homepage} />
-      <Route path="/show" exact component={Showpage} />
+      <Route path="/home" exact component={HomePage} />
+      <Route path="/show" exact component={ShowPage} />
       <Route path="/toppicks" exact component={TopPicks} />
       <Route path="/sortbyyear" exact component={SortByYear} />
       <Route path="/sortbygenre" exact component={SortByGenre} />
